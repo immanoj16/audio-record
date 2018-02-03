6afe81d84524b0d8ae49448611ed9aa992724ed8
immanoj16 <mmaharana14@gmail.com>
2018-02-03 13:00:06 +0530

    save the recording file

--- js/control.js
+++ js/control.js
@@ -1,34 +1,70 @@
+const fs = require('fs');
+const RecordRTC = require('recordrtc');
+const ffmpeg = require('fluent-ffmpeg');
+const StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
+
 let preview = document.getElementById("preview");
 let record = document.getElementById("record");
 let startButton = document.getElementById("start");
 let stopButton = document.getElementById("stop");
 
+function millisToMinutesAndSeconds(millis) {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
 
 function controls() {
   navigator.mediaDevices.getUserMedia({audio: true})
     .then(stream => {
-      var mediaRecorder = new MediaRecorder(stream);
-
+      var options = {
+        recorderType: StereoAudioRecorder,
+        mimeType: 'audio/wav',
+        autoWriteToDisk: true
+      };
+      var recordRTC = RecordRTC(stream, options);
       startButton.onclick = function () {
-        mediaRecorder.start();
+        recordRTC.startRecording();
         preview.srcObject = stream;
       }
 
-      var data = [];
-
-      mediaRecorder.ondataavailable = event => data.push(event.data);
-
       stopButton.onclick = function () {
-        mediaRecorder.stop();
-      }
+        recordRTC.stopRecording(function(audioURL) {
+          record.src = audioURL;
 
-      mediaRecorder.onstop = function () {
-        let recordedBlob = new Blob(data, { type: "audio/ogg; codecs=opus" });
-        record.src = URL.createObjectURL(recordedBlob);
+          var recordedBlob = recordRTC.getBlob();
+          var fileReader = new FileReader();
+          fileReader.onload = function () {
+            fs.writeFileSync('test.wav', Buffer(new Uint8Array(fileReader.result)));
+          };
+          fileReader.readAsArrayBuffer(recordedBlob);
+        })
+
+        while (true) {
+          const path = __dirname + './test.wav';
+          const current = Date.now();
+
+          if (fs.existsSync(path)) {
+            ffmpeg('./test.wav')
+              .toFormat('mp3')
+              .on('error', err => {
+                console.log('An error is occurred: ' + err.message);
+              })
+              .on('progress', progress => {
+                console.log('Processing: ' + progress.targetSize + ' KB converted');
+              })
+              .on('end', () => {
+                console.log('Finished!');
+                console.log(millisToMinutesAndSeconds(Date.now() - current));
+              })
+              .save('./test.mp3');
+
+            break;
+          } else {
+            console.log('file is not exist');
+          }
+        }
       }
-    })
-    .catch(err => {
-      console.log(err);
     })
 }
 
--- main.js
+++ main.js
@@ -5,12 +5,13 @@ const path = require('path')
 let win
 
 function createWindow() {
-  win = new BrowserWindow({width:500 , height: 200})
+  win = new BrowserWindow({width:800 , height: 600})
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
+  win.webContents.openDevTools();
 }
 
 app.on('ready', createWindow)
